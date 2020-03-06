5267a9c4cb407d8336b6543ebf3cc33a4285aa70
Josephlysaght <43279545+Josephlysaght@users.noreply.github.com>
2020-03-06 11:55:16 +0000

    Version 3 added.
    Outputs for parts 1 and 2 all done

--- Main_2.m
+++ Main_2.m
@@ -5,25 +5,37 @@ clc
 run('\\brookesf1\s59\17031059\GitHub\MVP_Assignment\template.m');
 
 %---------------------------------------------------Lets calculate the track parameters--------------------------------------------------------%
+%how many corners?
+NumCorners = length(Corner_radius);
+NumStraights = length(Straight_length);
+
+%Initize output struct
+Results.CSpeed = zeros(NumCorners,1);
+Results.CDrag = zeros(NumCorners,1);
+Results.CDownforce = zeros(NumCorners,1);
+Results.CTime = zeros(NumCorners,1);
+Corner_anglerad = zeros(NumCorners,1);
+Results.CDistance = zeros(NumCorners,1);
+
 %%Corner arc lenghts - arc lenght is the radius multiplied by the angle in radians 
-Corner_anglerad(1) = degtorad(Corner_angle(1)); %convert deg to radians T1
-Corner_anglerad(2) = degtorad(Corner_angle(2)); %convert deg to radians T2
-Corner_anglerad(3) = degtorad(Corner_angle(3)); %convert deg to radians T3
+for n=1:NumCorners
+    Corner_anglerad(n) = degtorad(Corner_angle(n)); %convert deg to radians
+end
 
 %calculates corner lenght (Arc lenght)
-Corner_length(1) = Corner_anglerad(1)*Corner_radius(1);
-Corner_length(2) = Corner_anglerad(2)*Corner_radius(2);
-Corner_length(3) = Corner_anglerad(3)*Corner_radius(3);
+for n=1:NumCorners
+    Results.CDistance(n) = Corner_anglerad(n)*Corner_radius(n);
+end
 
 %Points in each section
-C1points = round(Corner_length(1)/Delta_S);
+C1points = round(Results.CDistance(1)/Delta_S);
 S1points = round(Straight_length(1)/Delta_S) + C1points;
-C2points = round(Corner_length(2)/Delta_S) + S1points;
+C2points = round(Results.CDistance(2)/Delta_S) + S1points;
 S2points = round(Straight_length(2)/Delta_S) + C2points;
-C3points = round(Corner_length(2)/Delta_S) + S2points;
+C3points = round(Results.CDistance(2)/Delta_S) + S2points;
 
 %Calculationg overall track length
-Track.Length = Corner_length(1) + Corner_length(2) + Corner_length(3) + Straight_length(1) + Straight_length(2) + Straight_length(3);
+Track.Length = Results.CDistance(1) + Results.CDistance(2) + Results.CDistance(3) + Straight_length(1) + Straight_length(2) + Straight_length(3);
 
 %Calculating calculation points for track
 Track.Calc_points = round(Track.Length/Delta_S)+1;
@@ -32,6 +44,10 @@ Track.Calc_points = round(Track.Length/Delta_S)+1;
 Track.Distance = zeros(Track.Calc_points,1);
 Track.Section = zeros(Track.Calc_points,1);
 Track.Type = zeros(Track.Calc_points,1);
+Results.Speed = zeros(Track.Calc_points,1);
+Results.Distance = zeros(Track.Calc_points,1);
+Results.Time = zeros(Track.Calc_points,1);
+Results.Accel = zeros(Track.Calc_points,1);
 
 %filling in track data
 for n=1:Track.Calc_points
@@ -70,42 +86,36 @@ end
 
 %------------------------------------ Start Simulation Steps ------------------------------------------------%
 
-%Initize output struct
-Results.Speed = zeros(Track.Calc_points,1);
-Results.Distance = zeros(Track.Calc_points,1);
-Results.Time = zeros(Track.Calc_points,1);
-Results.Accel = zeros(Track.Calc_points,1);
-
 %Calculate corner speeds
-Results.C1Speed = sqrt((mu_lat*Corner_radius(1)*Mass*9.81)/(Mass-(ClA*mu_lat)));
-Results.C2Speed = sqrt((mu_lat*Corner_radius(2)*Mass*9.81)/(Mass-(ClA*mu_lat)));
-Results.C3Speed = sqrt((mu_lat*Corner_radius(3)*Mass*9.81)/(Mass-(ClA*mu_lat)));
+for n=1:NumCorners
+    Results.CSpeed(n) = sqrt((mu_lat*Corner_radius(n)*Mass*9.81)/(Mass-(ClA*mu_lat)));
+end
 
 %Calculate Aerodynamic drag during corner
-Results.C1Drag = CdA*(Results.C1Speed^2);
-Results.C2Drag = CdA*(Results.C2Speed^2);
-Results.C3Drag = CdA*(Results.C3Speed^2);
+for n=1:NumCorners
+    Results.CDrag(n) = CdA*(Results.CSpeed(n)^2);
+end
 
 %Calculate Aerodynamic downforce during corner
-Results.C1Downforce = ClA*(Results.C1Speed^2);
-Results.C2Downforce = ClA*(Results.C2Speed^2);
-Results.C3Downforce = ClA*(Results.C3Speed^2);
+for n=1:NumCorners
+    Results.CDownforce(n) = ClA*(Results.CSpeed(n)^2);
+end
 
 %Elapsed time in each turn 
-Results.C1Time = Corner_length(1)/Results.C1Speed;
-Results.C2Time = Corner_length(2)/Results.C2Speed;
-Results.C3Time = Corner_length(3)/Results.C3Speed;
+for n=1:NumCorners
+    Results.CTime(n) = Results.CDistance(n)/Results.CSpeed(n);
+end
 
 %Calculate Cornering speeds and straight line acceleration
 for n=2:Track.Calc_points 
     if Track.Type(n) == 'c'
         %calculate corner speed 
         if Track.Section(n) == 1
-            Results.Speed(n) = Results.C1Speed;
+            Results.Speed(n) = Results.CSpeed(1);
         elseif Track.Section(n) == 2
-            Results.Speed(n) = Results.C2Speed;
+            Results.Speed(n) = Results.CSpeed(2);
         else 
-            Results.Speed(n) = Results.C3Speed;
+            Results.Speed(n) = Results.CSpeed(3);
         end
         Results.Distance(n) = Results.Distance(n-1)+Delta_S;
     else 
@@ -168,30 +178,30 @@ ylabel('Speed (m/s)')
 %------------------- Outputs ---------------------------------------
 %Corner Speeds
 fprintf('----- Corner Speeds ----- \n');
-fprintf('C1 - %5.2f m/s \n',Results.C1Speed);
-fprintf('C2 - %5.2f m/s \n',Results.C2Speed);
-fprintf('C3 - %5.2f m/s \n\n',Results.C3Speed);
+for n=1:NumCorners
+    fprintf('C%d - %5.2f m/s \n',n,Results.CSpeed(n));
+end
 %Cornering Drag 
-fprintf('----- Aerodynamic Drag during corners----- \n');
-fprintf('C1 - %5.2f N \n',Results.C1Drag);
-fprintf('C2 - %5.2f N \n',Results.C2Drag);
-fprintf('C3 - %5.2f N \n\n',Results.C3Drag);
+fprintf('\n----- Aerodynamic Drag during corners----- \n');
+for n=1:NumCorners
+    fprintf('C%d - %5.2f N \n',n,Results.CDrag(n));
+end
 %Cornering downforce
-fprintf('----- Aerodynamic Downforce during corners ----- \n');
-fprintf('C1 - %5.2f N \n',Results.C1Downforce);
-fprintf('C2 - %5.2f N \n',Results.C2Downforce);
-fprintf('C3 - %5.2f N \n\n',Results.C3Downforce);
+fprintf('\n----- Aerodynamic Downforce during corners ----- \n');
+for n=1:NumCorners
+    fprintf('C%d - %5.2f N \n',n,Results.CDownforce(n));
+end
 %Corner Distances
-fprintf('----- Distance traveled in each corner ----- \n');
-fprintf('C1 - %5.2f m \n',Corner_length(1));
-fprintf('C2 - %5.2f m \n',Corner_length(2));
-fprintf('C3 - %5.2f m \n\n',Corner_length(3));
+fprintf('\n----- Distance traveled in each corner ----- \n');
+for n=1:NumCorners
+    fprintf('C%d - %5.2f m \n',n,Results.CDistance(n));
+end
 %Corner Times
-fprintf('----- Time elapsed in each corner ----- \n');
-fprintf('C1 - %5.2f s \n',Results.C1Time);
-fprintf('C2 - %5.2f s \n',Results.C2Time);
-fprintf('C3 - %5.2f s \n\n',Results.C2Time);
+fprintf('\n----- Time elapsed in each corner ----- \n');
+for n=1:NumCorners
+    fprintf('C%d - %5.2f s \n',n,Results.CTime(n));
+end
 
 %Removing unnessesary varibles
-clear C1points C2points C3points S1points S2points S3points
+clear C1points C2points C3points S1points S2points S3points Corner_anglerad
 
